44086b5330844e91c6b86800f901f674e81c422a
HelenGladkih <gladkihen@mail.ru>
2025-07-18 09:43:07 +0500

    В методах классов добавлен параметр 'self'.

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -6,17 +6,17 @@ from helpers import *
 class OrderMethods:
 
     @allure.step('Создать заказ')
-    def create_order(payload):
+    def create_order(self, payload):
         response = requests.post(ORDERS_URL, json = payload)
         return response.status_code, response.json()
         
     @allure.step('Получить список заказов')
-    def get_list_orders():
+    def get_list_orders(self):
         response = requests.get(ORDERS_URL)
         return response.status_code, response.text
 
     @allure.step('Получить отсортированный список заказов')
-    def get_list_orders_sort(payload):
+    def get_list_orders_sort(self, payload):
         response = requests.get(ORDERS_URL, params = payload)
         return response.status_code, response.json()
     
--- tests/test_courier.py
+++ tests/test_courier.py
@@ -9,21 +9,21 @@ class TestCourierCreate:
     @allure.title('Позитивный сценарий создания курьера: передаем все обязательные поля (логин, пароль).')
     @allure.description('ОР: Курьер успешно создан.')
     def test_create_courier_login_password_created_successfully(self):
-        status_code, response_data = CourierMethods.create_courier()
+        status_code, response_data = CourierMethods().create_courier()
         assert status_code == 201 and response_data['ok'] == True
 
     @allure.title('Негативный сценарий создания курьера: логин уже есть в системе.')
     @allure.description('ОР: Нельзя создать двух одинаковых курьеров, запрос возвращает ошибку.')
     @pytest.mark.parametrize('payload', [CourierAuthData.COURIER_AUTH_DATA_1])
     def test_create_courier_duplicate_login_error_409(self, payload):
-        status_code, response_data = CourierMethods.create_courier(payload)
+        status_code, response_data = CourierMethods().create_courier(payload)
         assert status_code == 409 and response_data['message'] == RegErrors.DUPLICATE_LOGIN
 
     @allure.title('Негативный сценарий создания курьера: передаем пустой логин или пустой пароль.')
     @allure.description('ОР: Курьер не создан, запрос возвращает ошибку.')
     @pytest.mark.parametrize('payload', [CourierAuthData.COURIER_AUTH_DATA_2, CourierAuthData.COURIER_AUTH_DATA_3])
     def test_create_courier_missing_login_or_password_error_400(self, payload):
-        status_code, response_data = CourierMethods.create_courier(payload)
+        status_code, response_data = CourierMethods().create_courier(payload)
         assert status_code == 400 and response_data['message'] == RegErrors.MISSING_DATA
 
 class TestCourierLogin:
@@ -32,19 +32,19 @@ class TestCourierLogin:
     @allure.description('ОР: Курьер успешно авторизирован в системе, запрос возвращает id курьера')
     @pytest.mark.parametrize("payload", [CourierLoginData.COURIER_LOGIN_DATA_1])
     def test_login_courier_valid_credentials_success_200(self, payload):
-        status_code, response_data = CourierMethods.login_courier(payload)
+        status_code, response_data = CourierMethods().login_courier(payload)
         assert status_code == 200 and response_data['id'] is not None
 
     @allure.title('Негативный сценарий авторизации курьера: передаем несуществующий логин или неверный пароль')
     @allure.description('ОР: Курьер не авторизирован в системе, запрос возвращает ошибку.')
     @pytest.mark.parametrize("payload", [CourierLoginData.COURIER_LOGIN_DATA_2, CourierLoginData.COURIER_LOGIN_DATA_3])
     def test_login_courier_invalid_credentials_not_found_404(self, payload):
-        status_code, response_data = CourierMethods.login_courier(payload)
+        status_code, response_data = CourierMethods().login_courier(payload)
         assert status_code == 404 and response_data["message"] == LoginErrors.USER_NOT_FOUND
 
     @allure.title('Негативный сценарий авторизации курьера: передаем пустой логин или пустой пароль.')
     @allure.description('ОР: Курьер не авторизирован в системе, запрос возвращает ошибку.')
     @pytest.mark.parametrize("payload", [CourierLoginData.COURIER_LOGIN_DATA_4, CourierLoginData.COURIER_LOGIN_DATA_5])
     def test_login_courier_empty_login_or_password_bad_request_400(self, payload):
-        status_code, response_data = CourierMethods.login_courier(payload)
+        status_code, response_data = CourierMethods().login_courier(payload)
         assert status_code == 400 and response_data["message"] == LoginErrors.MISSING_AUTH_DATA
